171ad830a66ad7238ccccb1a7de594c25718d574
Abhineet Tamrakar <abhineet97@gmail.com>
2016-05-29 07:40:32 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+#WebRTC-Chess
+
+Chess moves travelling through WebRTC.
+
+##Libraries Used
+
+- [Chess.js](https://github.com/jhlywa/chess.js) for game login.
+- [moroshko/chessboard](https://github.com/moroshko/chessboard) for the Chess board.
+- [PeerJS](http://peerjs.com) for WebRTC.
+
+##License
+
+[MIT](/LICENSE.md)
---
+++ index.js
@@ -0,0 +1,212 @@
+/*global ChessBoard*/
+'use strict';
+
+document.addEventListener('DOMContentLoaded', main);
+
+function getOrientation() {
+  if (window.location.hash && window.location.hash.length !== 0) return 'b';
+  else return 'w';
+}
+
+function main() {
+  require('./lib/chessboard/chessboard.js');
+  var Chess = require('chess.js').Chess;
+  var Peer = require('peerjs');
+
+  var board, game, onSquareClick, onData, onOpen, move, checkGameStatus;
+
+  var peer = new Peer({
+    key: '618b8av1yey4lsor'
+  });
+  var user = {},
+    friend = {},
+    connected = false;
+
+  onSquareClick = function(clickedSquare, selectedSquares) {
+    if (!checkTurn()) {
+      board.unselectSquare(clickedSquare);
+      return;
+    }
+
+    if (selectedSquares.length === 0) {
+      if (game.moves({
+          square: clickedSquare
+        }).length > 0) {
+        board.selectSquare(clickedSquare);
+      }
+
+      return;
+    }
+
+    var selectedSquare = selectedSquares[0];
+
+    if (clickedSquare === selectedSquare) {
+      board.unselectSquare(clickedSquare);
+      return;
+    }
+
+    var clickedPieceObject = game.get(clickedSquare);
+    var selectedPieceObject = game.get(selectedSquare);
+
+    if (clickedPieceObject && (clickedPieceObject.color === selectedPieceObject.color)) {
+      board.unselectSquare(clickedSquare);
+      return;
+    }
+
+    var legalMoves = game.moves({
+      square: selectedSquare,
+      verbose: true
+    });
+    var isMoveLegal = legalMoves.filter(function(move) {
+      return move.to === clickedSquare;
+    }).length > 0;
+
+    if (!isMoveLegal) {
+      status('Invalid move. Try Again.')
+      return;
+    }
+
+    if (selectedPieceObject.type === 'p' && (clickedSquare[1] === '1' || clickedSquare[1] === '8')) { // Promotion
+      board.askPromotion(selectedPieceObject.color, function(shortPiece) {
+        move(selectedSquare, clickedSquare, shortPiece);
+      });
+    }
+    else {
+      move(selectedSquare, clickedSquare);
+    }
+  };
+
+  move = function(from, to, promotionShortPiece) {
+    game.move({
+      from: from,
+      to: to,
+      promotion: promotionShortPiece
+    });
+
+    board.setPosition(game.fen());
+
+    sendMove();
+    board.unselectAllSquares();
+  };
+
+  var sendMove = function() {
+    friend.dataConnection.send(game.fen());
+    checkGameStatus();
+  };
+
+  checkGameStatus = function() {
+    if (game.game_over()) {
+      if (game.in_draw()) {
+        status('Game Over. It\'s a DRAW.');
+      }
+      status('Game Over. You have ' + (game.turn() === user.color ? 'LOST' : 'WON'));
+    }
+    else if (game.in_check()) {
+      status((game.turn() === user.color ? 'You are' : 'Opponent is') + ' in CHECK.');
+    }
+    else {
+      status((game.turn() === user.color ? 'Your' : 'Opponent\'s') + ' turn.');
+    }
+  };
+
+  var dialog = document.querySelector('dialog');
+
+  var setModal = function(string) {
+    dialog.innerHTML = string;
+    dialog.showModal();
+  };
+
+  var checkTurn = function() {
+    if (game.game_over()) {
+      checkGameStatus();
+      return false;
+    }
+    else if (game.turn() !== user.color) {
+      status('Not your turn');
+      return false;
+    }
+    else if (!connected) {
+      if(peer.disconnected){
+        status('Connection to your opponent has been lost.');
+      }else{
+        status('Please wait while we connect to your opponent...');
+      }
+      return false;
+    }
+    return true;
+  };
+
+  var status = function(string) {
+    document.getElementById('msg').innerHTML = string;
+  };
+
+  onData = function(data) {
+    console.log('Recieved', data);
+    game.load(data);
+    board.setPosition(game.fen());
+    checkGameStatus();
+  };
+
+  onOpen = function(dataConnection) {
+    if (user.color === 'w') {
+      dialog.close();
+      status('Connected to opponent, play your turn.');
+    }
+    else {
+      status('Connected to opponent, waiting for him/her to play...');
+    }
+
+    connected = true;
+    friend.dataConnection = dataConnection;
+  };
+
+  peer.on('open', function(id) {
+    status('Waiting for opponent...');
+    if (window.location.hash.length !== 0) {
+      user = {
+        id: id,
+        color: 'b'
+      };
+
+      friend = {
+        id: window.location.hash.split('#').pop(),
+        color: 'w'
+      };
+
+      var dataConnection = peer.connect(friend.id);
+      status('Connecting to opponent...');
+
+      dataConnection.on('open', function() {
+        onOpen(dataConnection);
+      });
+
+      dataConnection.on('data', onData);
+    }
+    else {
+      user = {
+        color: 'w',
+        id: id
+      };
+
+      friend = {
+        color: 'b'
+      };
+
+      setModal('Share this URL to begin<br><input onclick="this.select();" value="' + window.location.href + '#' + user.id + '">');
+    }
+  });
+
+  peer.on('connection', function(dataConnection) {
+    dataConnection.on('open', function() {
+      onOpen(dataConnection);
+    });
+
+    dataConnection.on('data', onData);
+  });
+
+  game = Chess();
+  board = new ChessBoard('board', {
+    onSquareClick: onSquareClick,
+    orientation: getOrientation()
+  });
+}
