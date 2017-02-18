e0b344d76e57d65b761a2c81596783dd28a82a7a
Abhineet Tamrakar <abhineet97@users.noreply.github.com>
2017-02-18 06:42:57 -0500

    Add gulp

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp');
+var inlinesource = require('gulp-inline-source');
+var htmlmin = require('gulp-htmlmin');
+var ghPages = require('gulp-gh-pages');
+
+gulp.task('build', function() {
+  var options = {};
+  options.inlinesource = {
+    compress: true
+  };
+
+  var stream = gulp.src('./index.html')
+    .pipe(inlinesource({compress: true}))
+    .pipe(htmlmin({
+      removeComments: true,
+      collapseWhitespace: true,
+      collapseBooleanAttributes: true,
+      removeAttributeQuotes: true,
+      removeRedundantAttributes: true,
+      removeEmptyAttributes: true,
+      removeScriptTypeAttributes: true,
+      removeStyleLinkTypeAttributes: true,
+      removeOptionalTags: true
+    }))
+    .pipe(gulp.dest('./dist'));
+  
+  return stream;
+});
+
+gulp.task('deploy', ['build'], function() {
+  gulp.src('./dist/index.html')
+  .pipe(ghPages())
+});
+
+gulp.task('clean')
